42964c33c7951e6a89a6ad3ce0a692f64babd0b6
GustavodePaulaGorges <gustavodep.gorges@gmail.com>
2023-04-14 13:12:05 -0300

    Cors

--- livraria/views.py
+++ livraria/views.py
@@ -1,8 +1,20 @@
 from rest_framework.viewsets import ModelViewSet
 
-from livraria.models import Categoria
-from livraria.serializers import CategoriaSerializer
+from livraria.models import Categoria, Editora, Livro
+from livraria.serializers import CategoriaSerializer, EditoraSerializer, LivroDetailSerializer, LivroSerializer
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+
+class EditoraViewSet(ModelViewSet):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializer
+
+class LivroViewSet(ModelViewSet):
+    queryset = Livro.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return LivroDetailSerializer
+        return LivroSerializer
