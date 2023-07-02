e671c96a1c9c5cd189b5853bed61a1827648ca9e
GustavodePaulaGorges <gustavodep.gorges@gmail.com>
2023-07-02 19:04:44 -0300

    Autenticação com SimpleJWT

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 from rest_framework.routers import DefaultRouter
 
@@ -11,6 +16,8 @@ router.register(r"editoras", EditoraViewSet)
 router.register(r"livros", LivroViewSet)
 
 urlpatterns = [
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path('admin/', admin.site.urls),
     path("", include(router.urls)),
 ]
--- livraria/views.py
+++ livraria/views.py
@@ -1,9 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 from livraria.models import Categoria, Editora, Livro
 from livraria.serializers import CategoriaSerializer, EditoraSerializer, LivroDetailSerializer, LivroSerializer, LivroListSerializer
 
 class CategoriaViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
 
